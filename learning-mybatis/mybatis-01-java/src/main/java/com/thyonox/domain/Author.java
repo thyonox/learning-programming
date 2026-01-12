0e31385e0b4b346673710e280099df7708a6245f
thyonox <thyonox@gmail.com>
2026-01-12 15:45:22 +0800

    Add learning-mybatis dir

---
+++ learning-mybatis/mybatis-01-java/src/main/java/com/thyonox/domain/Author.java
@@ -0,0 +1,31 @@
+package com.thyonox.domain;
+
+public class Author {
+
+    private Long authorId;
+
+    private String authorName;
+
+    public Author() {
+    }
+
+    public Author(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+}
---
+++ learning-mybatis/mybatis-01-java/src/main/java/com/thyonox/domain/Book.java
@@ -0,0 +1,52 @@
+package com.thyonox.domain;
+
+public class Book {
+
+    private Long bookId;
+
+    private String title;
+
+    private Long authorId;
+
+    private String description;
+
+    public Book() {
+    }
+
+    public Book(String title, Long authorId) {
+        this.title = title;
+        this.authorId = authorId;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
