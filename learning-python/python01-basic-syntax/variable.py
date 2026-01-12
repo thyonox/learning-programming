1e1f760dfbd53b3a4a4dde7f6d714d6c64175ef9
thyonox <thyonox@gmail.com>
2026-01-13 01:01:24 +0800

    Add learning-python dir

---
+++ learning-python/python01-basic-syntax/variable.py
@@ -0,0 +1,11 @@
+x = 10
+name = "John Doe"
+pi = 3.14
+is_valid = True
+
+print(f"x: {x}, name: {name}, pi: {pi}, is_valid: {is_valid}")
+
+if (x > 5):
+    print("x is greater than 5")
+else:
+    print("x is not greater than 5")
