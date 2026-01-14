4797a23c633a73b76156ceff76f442ce6b4967d5
thyonox <thyonox@gmail.com>
2026-01-14 13:52:43 +0800

    Add learning-spring-boot dir

---
+++ learning-spring-boot/src/main/java/org/thyonix/LearningSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.thyonix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSpringBootApplication.class, args);
+    }
+
+}
---
+++ learning-spring-boot/src/test/java/org/thyonix/LearningSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thyonix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
