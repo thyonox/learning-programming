3c3aa6f5ac890c6a70c5160767e94140fa629135
thyonox <thyonox@gmail.com>
2026-02-27 22:52:11 +0800

    feat（JavaScript）：更新JS代码片段

--- learning-javascript/02-function/01-definition.js
+++ learning-javascript/02-function/01-definition.js
@@ -17,3 +17,25 @@ console.log(multiply(5, 6));
 // 4.构造函数
 const sum2 = new Function('a', 'b', 'return a + b');
 console.log(sum2(7, 8));
+
+// 5.方法简写
+const obj = {
+    add(a, b) {
+        return a + b;
+    }
+}
+console.log(obj.add(9, 10));
+
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+
+    greet() {
+        return `Hello, my name is ${this.name}`;
+    }
+}
+const person = new Person('Alice');
+console.log(person.greet());
+
+
