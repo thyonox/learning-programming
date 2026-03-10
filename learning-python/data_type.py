cebb2d58e4b67206a95124ad2fe23163dfda5d9e
thyonox <thyonox@gmail.com>
2026-03-10 18:25:19 +0800

    feat（learning-python）：更新文件夹

--- learning-python/data_type.py
+++
@@ -1,2 +0,0 @@
-num = 3 + 4j
-print(type(num))
---
+++ learning-python/python-basic/data_type.py
@@ -0,0 +1,18 @@
+num = 3 + 4j
+print(type(num))
+num = 3.14
+print(type(num))
+num = 42
+print(type(num))
+text = "hello"
+print(type(text))
+is_valid = True
+print(type(is_valid))
+my_list = [1, 2, 3]
+print(type(my_list))
+my_tuple = (1, 2, 3)
+print(type(my_tuple))
+my_set = {1, 2, 3}
+print(type(my_set))
+my_dict = {"name": "Alice", "age": 30}
+print(type(my_dict))
